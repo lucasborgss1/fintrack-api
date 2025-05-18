dff153779f42c579c069e8b6583a18bb4b78b5d2
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-05-18 18:18:38 -0300

    chore: atualizando repository, configuracoes e dependencias no pom.xml

--- src/main/java/com/lucas/fintrack/api/domain/repository/TransactionRepository.java
+++ src/main/java/com/lucas/fintrack/api/domain/repository/TransactionRepository.java
@@ -1,8 +1,7 @@
 package com.lucas.fintrack.api.domain.repository;
 
-import com.lucas.fintrack.api.domain.model.Transaction;
+import com.lucas.fintrack.api.domain.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
 }
